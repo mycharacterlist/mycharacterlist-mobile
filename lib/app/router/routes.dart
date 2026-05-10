ae7131dfb8630c10d32c1414023fd00ad3d19301
lZolotoy <daroktor@gmail.com>
2026-05-10 13:53:34 +0200

    Add route definitions

--- lib/app/router/routes.dart
+++ lib/app/router/routes.dart
@@ -1,6 +1,7 @@
 class AppRoutes {
   const AppRoutes._();
 
+  // App routes.
   static const home = '/';
   static const lists = '/lists';
   static const rankingList = '/lists/:id';
@@ -8,6 +9,11 @@ class AppRoutes {
   static const characterCreate = '/characters/create';
   static const library = '/library';
 
+  // Dev routes.
+  static const dev = '/dev';
+  static const homePreview = '/home-preview';
+
+  // Route builders.
   static String rankingListById(String id) => '/lists/$id';
   static String characterById(String id) => '/characters/$id';
 }
