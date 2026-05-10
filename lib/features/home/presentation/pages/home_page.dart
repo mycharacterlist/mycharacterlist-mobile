c34a2b9fe8a825c4e0d139204ad33b1db8d5b545
Timtopgg <swir4ok@gmail.com>
2026-05-10 22:13:32 +0200

    Add text home (testing commit)

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Main menu')),
+      appBar: AppBar(title: const Text('home!')),
       body: const Center(
         child: Text('Home page'),
       ),
