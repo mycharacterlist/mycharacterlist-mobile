003792e9b73aeca3dd9fe32db366b10ba52b2716
Timtopgg <swir4ok@gmail.com>
2026-05-18 23:52:36 +0200

    Created button "Create new" without functionality

--- lib/features/ranking_lists/presentation/pages/lists_page.dart
+++ lib/features/ranking_lists/presentation/pages/lists_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import '../../../../app/widgets/app_appbar.dart';
+import '../widgets/CreateNew_button.dart';
 
 class ListsPage extends StatelessWidget {
   const ListsPage({super.key});
@@ -8,19 +10,40 @@ class ListsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(
-          title: 'My Lists',
-          backgroundColor: const Color(0xFF0E2432),
-          backButtonColor: const Color(0xFFB60894),
-          titleColor: const Color(0xFFB60894),
+        title: 'My Lists',
+
+        backgroundColor: const Color(0xFF0E2432),
+
+        backButtonColor: const Color(0xFFB60894),
+
+        titleColor: const Color(0xFFB60894),
       ),
+
       body: Container(
         width: double.infinity,
         height: double.infinity,
 
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/images/ListsPage_bg.png'),
+            image: AssetImage(
+              'assets/images/ListsPage_bg.png',
+            ),
+
             fit: BoxFit.cover,
+          ),
+        ),
+
+        child: Align(
+          alignment: Alignment.bottomCenter,
+
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 40),
+
+            child: CreateNewButton(
+              text: 'Create new',
+
+              onPressed: () {},
+            ),
           ),
         ),
       ),
