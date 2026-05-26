4b95f49f30daa8e52a67b13b5d3876728e2302e6
Timtopgg <swir4ok@gmail.com>
2026-05-26 21:11:23 +0200

    1) Created Library page 2) Added appbar 3) Added button "+"

--- lib/features/library/presentation/pages/library_page.dart
+++ lib/features/library/presentation/pages/library_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../../../../app/widgets/app_appbar.dart';
+import '../widgets/Plus_button.dart';
 
 class LibraryPage extends StatelessWidget {
   const LibraryPage({super.key});
@@ -6,8 +8,48 @@ class LibraryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Library')),
-      body: const Center(child: Text('Library page')),
+      appBar: CustomAppBar(
+        title: 'Library',
+
+        backgroundColor:
+        const Color(0xFF1A4043),
+
+        backButtonColor:
+        const Color(0xFF009768),
+
+        titleColor:
+        const Color(0xFF4CB897),
+      ),
+
+      body: Stack(
+        children: [
+
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/Library_bg.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 35,
+
+            child: Center(
+              child: PlusButton(
+                icon: const Icon(
+                  Icons.add,
+                  color: Colors.black,
+                  size: 45,
+                ),
+
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
