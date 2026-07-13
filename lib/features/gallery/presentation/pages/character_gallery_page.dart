285a16c0d11c071bf2e5aac6720c96c4e5963507
Timtopgg <swir4ok@gmail.com>
2026-07-13 02:49:58 +0200

    1) Created gallery page 2) Created App bar (finished) 3) Added gray rectangle as background

--- lib/features/gallery/presentation/pages/character_gallery_page.dart
+++ lib/features/gallery/presentation/pages/character_gallery_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mycharacterlist/app/widgets/layout/app_appbar.dart';
 
-class CharacterGalleryPage extends ConsumerWidget {
+class CharacterGalleryPage
+    extends ConsumerWidget {
+
   const CharacterGalleryPage({
     super.key,
     required this.characterId,
@@ -10,19 +13,80 @@ class CharacterGalleryPage extends ConsumerWidget {
   final String characterId;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(
+      BuildContext context,
+      WidgetRef ref,
+      ) {
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Gallery'),
+
+      appBar: CustomAppBar(
+        title: 'Gallery page',
+        backgroundColor: const Color(0xFF024818),
+        titleColor: const Color(0xFFFFFFFF),
+        backButtonColor: Colors.black,
+
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Text(
-            'Character gallery',
-            textAlign: TextAlign.center,
+
+      body: Stack(
+        children: [
+
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/Gallery_bg.jpg',
+
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
+
+          Center(
+            child: SizedBox(
+              width:
+              MediaQuery.of(context)
+                  .size
+                  .width * 0.95,
+
+              height:
+              MediaQuery.of(context)
+                  .size
+                  .height * 0.87,
+
+              child: Stack(
+                children: [
+
+                  Positioned.fill(
+                    child: Image.asset(
+                      'assets/images/cropped_rectangle.png',
+
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+
+                  Positioned(
+                    top: 0,
+                    left: 5,
+                    right: 5,
+                    bottom: 20,
+
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+
+                          const SizedBox(
+                            height: 5,
+                          ),
+
+
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
